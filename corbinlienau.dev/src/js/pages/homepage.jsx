db33630cfb2cf350bc8429f7df2238ea5500d973
garnetraven <corbinchristopher1@gmail.com>
2024-03-08 18:22:54 -0700

    Init setup

---
+++ corbinlienau.dev/src/js/pages/homepage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Footer from "../components/footer"
+
+const Homepage = () => {
+
+  return (
+    <div>
+      <div>
+        <h1>title</h1>
+      </div>
+      <div className="page-footer">
+	<Footer />
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;

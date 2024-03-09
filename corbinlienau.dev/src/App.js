3b9248c5a945d825cac7a0a00f593c1632e2ae0f
garnetraven <corbinchristopher1@gmail.com>
2024-03-08 18:26:44 -0700

    Update pages to router

--- corbinlienau.dev/src/App.js
+++ corbinlienau.dev/src/App.js
@@ -1,6 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 
 import Homepage from "./js/pages/homepage";
+import About from "./js/pages/about";
+import Contact from "./js/pages/contact";
+import Projects from "./js/pages/projects";
+import Notfound from "./js/pages/404";
 
 import './css/App.css';
 

39b425d2a467ec670e609899044fc77875250536
garnetraven <corbinchristopher1@gmail.com>
2024-03-10 14:23:42 -0700

    homepage intro styling

--- corbinlienau.dev/src/js/data/profile.js
+++ corbinlienau.dev/src/js/data/profile.js
@@ -1,14 +1,18 @@
 const INFO = {
   primary: {
-
+    title: "Corbin's Website",
+    name: "Corbin Lienau",
+    email: "corbinchristopher1@gmail.com",
   },
 
   socials: {
-
+    github: "https://github.com/garnetraven/",
+    linkedin: "https://linkedin/in/corbinlienau/",
   },
+
   homepage: {
     title: "Corbin Lienau - Software Engineer",
-    description: "I am a software engineer with an interest in backend web development and real time simulation. I have experience with web development, agile practices, and diverse technologies and languages. I find purpose in bringing my ideas to fruition and simplifying complex problems. My passion lies in continuous self-improvement while contributing effectively to large-scale projects",
+    description: "I am a software engineer with an interest in backend web development and real time simulation. I have experience with web development, agile practices, and diverse technologies and languages. I find purpose in bringing my ideas to fruition and simplifying complex problems. My passion lies in continuous self-improvement while contributing effectively to large-scale projects.",
   }
 };
 
--- corbinlienau.dev/src/js/pages/homepage.jsx
+++ corbinlienau.dev/src/js/pages/homepage.jsx
@@ -3,6 +3,14 @@ import { React } from "react";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
 
+import { faMailBulk } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faLinkedin,
+	faGithub,
+} from "@fortawesome/free-brands-svg-icons";
+
+
 import INFO from "../data/profile"
 
 import "../../css/homepage.css";
@@ -23,7 +31,9 @@ const Homepage = () => {
             </div>
           </div>
           <div className="homepage-socials">
-        
+            <a href={INFO.socials.github} target="_blank"><FontAwesomeIcon icon={faGithub} className="homepage-social-icon"/></a>
+            <a href={INFO.socials.linkedin} target="_blank"><FontAwesomeIcon icon={faLinkedin} className="homepage-social-icon"/></a>
+            <a href={`https://mail.google.com/mail/?view=cm&source=mailto&to=${INFO.primary.email}`} target="_blank"><FontAwesomeIcon icon={faMailBulk} className="homepage-social-icon"/></a>
           </div>
           <div className="homepage-projects">
 

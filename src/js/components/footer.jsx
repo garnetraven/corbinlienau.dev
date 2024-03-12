3a906587885a90582da9430ba2ab285ded580f47
garnetraven <corbinchristopher1@gmail.com>
2024-03-12 11:51:29 -0700

    update footer styling

--- src/js/components/footer.jsx
+++ src/js/components/footer.jsx
@@ -8,18 +8,18 @@ const Footer = () => {
     <div className="footer">
       <div className="footer-links">
         <ul className="footer-nav-link-list">
-	  <li className="footer-nav-link-item">
-	    <Link to="/">Home</Link>
-	  </li>
-	  <li className="footer-nav-link-item">
-	    <Link to="/about">About</Link>
-	  </li>
-	  <li className="footer-nav-link-item">
-	    <Link to="/projects">Projects</Link>
-	  </li>
-	  <li className="footer-nav-link-item">
-	    <Link to="/contact">Contact</Link>
-	  </li>
+					<li className="footer-nav-link-item">
+						<Link to="/">Home</Link>
+					</li>
+					<li className="footer-nav-link-item">
+						<Link to="/about">About</Link>
+					</li>
+					<li className="footer-nav-link-item">
+						<Link to="/projects">Projects</Link>
+					</li>
+					<li className="footer-nav-link-item">
+						<Link to="/contact">Contact</Link>
+					</li>
         </ul>
       </div>
     </div>

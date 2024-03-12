a79cb8eaf99097e5820cc2b4d59a767818fb974c
garnetraven <corbinchristopher1@gmail.com>
2024-03-12 14:24:33 -0700

    Update homepage description

--- src/js/data/profile.js
+++ src/js/data/profile.js
@@ -12,7 +12,7 @@ const INFO = {
 
   homepage: {
     title: "Corbin Lienau - Software Engineer",
-    description: "I am a software engineer with an interest in backend web development and real time simulation. I have experience with web development, agile practices, and diverse technologies and languages. I find purpose in bringing my ideas to fruition and simplifying complex problems. My passion lies in continuous self-improvement while contributing effectively to large-scale projects.",
+    description: "I am a software engineer with an interest in backend web development and effective hardware solutions. I have experience with web development, agile practices, and diverse technologies and languages. I find purpose in bringing my ideas to fruition and simplifying complex problems. My passion lies in continuous self-improvement while contributing effectively to large-scale projects.",
   },
 
   about: {

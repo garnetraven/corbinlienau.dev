c633e8a27b6efc85dc3178500d0ce3c650e9361b
garnetraven <corbinchristopher1@gmail.com>
2024-03-12 14:22:09 -0700

    built about page structure

--- src/js/data/profile.js
+++ src/js/data/profile.js
@@ -13,7 +13,12 @@ const INFO = {
   homepage: {
     title: "Corbin Lienau - Software Engineer",
     description: "I am a software engineer with an interest in backend web development and real time simulation. I have experience with web development, agile practices, and diverse technologies and languages. I find purpose in bringing my ideas to fruition and simplifying complex problems. My passion lies in continuous self-improvement while contributing effectively to large-scale projects.",
-  }
+  },
+
+  about: {
+    title:"I'm Corbin Lienau. I find great joy in witnessing the concepts in my mind materialize through the code I create.",
+    description:"As a software engineer currently enrolled in the Software Engineering program at Arizona State University, my drive stems from crafting practical solutions with my code. While I have a particular affinity for backend systems and high-performance hardware solutions, I'm eager to embrace any opportunity that comes my way. My enthusiasm for software engineering is rooted in the ability to translate my conceptualizations into tangible results through coding. My aspiration is to delve deeper into this field, leveraging my skills to enhance people's lives through innovative software solutions.",
+  },
 };
 
 export default INFO;
--- src/js/pages/about.jsx
+++ src/js/pages/about.jsx
@@ -2,8 +2,11 @@ import { React } from "react";
 
 import Footer from "../components/footer"
 import Navbar from "../components/navbar";
+import Socials from "../components/socials";
 
 import "../../css/about.css";
+
+import INFO from "../data/profile";
 
 const About = () => {
 
@@ -12,6 +15,23 @@ const About = () => {
       <Navbar active="about"/>
       <div className="content-wrapper">
         <div className="about-container">
+          <div className="about-content">
+            <div className="about-content-left-side">
+              <div className="title about-title">
+                {INFO.about.title}
+              </div>
+
+              <div className="subtitle about-subtitle">
+                {INFO.about.description}
+              </div>
+            </div>
+
+            <div className="about-content-right-side">
+              <div className="about-socials">
+                <Socials />
+              </div>
+            </div>
+          </div>
           <div className="page-footer">
             <Footer />
           </div>

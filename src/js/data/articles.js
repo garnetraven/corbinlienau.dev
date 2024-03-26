05debebe9e9c8ff2eec44c10e4a6d19d576a98d8
garnetraven <corbinchristopher1@gmail.com>
2024-03-25 18:06:22 -0700

    update articles page and article styling

--- src/js/data/articles.js
+++ src/js/data/articles.js
@@ -3,7 +3,7 @@ import React from "react";
 function mediaserver() {
 	return {
 		codeLink: '',
-		coverImage: "",
+		coverImage: "https://cdn-icons-png.flaticon.com/128/919/919853.png",
 		date: "February 2024",
 		title: "Dockerized Home Media Server",
 		description:
@@ -28,14 +28,59 @@ function mediaserver() {
 			<div className="article-content">
 				<div className="paragraph"><p></p></div>
 				<img
-						src=""
+						src="https://cdn-icons-png.flaticon.com/128/919/919853.png"
 						alt="random"
 						className="randImage"
 					/>
 					<p>Hello!</p>
 					<h1>Hello!</h1>
 				<img
-						src=""
+						src="https://cdn-icons-png.flaticon.com/128/919/919853.png"
+						alt="group"
+						className="randImage"
+					/>
+			</div>
+			</React.Fragment>
+		),
+	};
+}
+
+function mediaserver2() {
+	return {
+		codeLink: '',
+		coverImage: "https://cdn-icons-png.flaticon.com/128/919/919853.png",
+		date: "February 2024",
+		title: "Dockerized Home Media Server",
+		description:
+			"Setting up a media server using Jellyfin, Sonarr, Radarr in Docker.",
+			style: `
+			.article-content {
+				display: flex;
+				flex-direction: column;
+				align-items: center;
+			}
+
+
+			.randImage {
+				width: 50%;
+				align-self: center;
+			}
+			`,
+		keywords: [
+		],
+		body: (
+			<React.Fragment>
+			<div className="article-content">
+				<div className="paragraph"><p></p></div>
+				<img
+						src="https://cdn-icons-png.flaticon.com/128/919/919853.png"
+						alt="random"
+						className="randImage"
+					/>
+					<p>Hello!</p>
+					<h1>Hello!</h1>
+				<img
+						src="https://cdn-icons-png.flaticon.com/128/919/919853.png"
 						alt="group"
 						className="randImage"
 					/>
@@ -45,6 +90,6 @@ function mediaserver() {
 	};
 }
 	
-const myArticles = [mediaserver];
+const myArticles = [mediaserver, mediaserver2];
 
 export default myArticles;
